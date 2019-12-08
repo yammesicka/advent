c27b89925d15076706268bf5738a6af634a25e02
Yam Mesicka <yammesicka@gmail.com>
2019-12-08 21:32:01 +0200

    Organize the code a bit

--- 2019/8/pretty.py
+++ 2019/8/pretty.py
@@ -1,24 +1,11 @@
 import itertools
-from typing import Tuple
+from typing import List, Tuple
 
 
 HEIGHT, WIDTH = 6, 25
 PICTURE_SIZE = HEIGHT * WIDTH
 
 
-with open('input.txt') as picture_format:
-    picture = picture_format.read().strip()
-
-layer_starts = range(0, len(picture), PICTURE_SIZE)
-layers = [picture[i:i + PICTURE_SIZE] for i in layer_starts]
-
-
-# Part 1
-fewest_zeros = min(layers, key=lambda layer: layer.count('0'))
-print(fewest_zeros.count('1') * fewest_zeros.count('2'))
-
-
-# Part 2
 def parse_pixel_from_layers(layers: Tuple[str, ...]) -> str:
     try:
         return next(itertools.dropwhile('2'.__eq__, layers))
@@ -26,6 +13,22 @@ def parse_pixel_from_layers(layers: Tuple[str, ...]) -> str:
         return '2'
 
 
+def get_input() -> str:
+    with open('input.txt') as picture_format:
+        return picture_format.read().strip()
+
+
+def get_layers(image_blob: str) -> List[str]:
+    layer_starts = range(0, len(image_blob), PICTURE_SIZE)
+    return [image_blob[i:i + PICTURE_SIZE] for i in layer_starts]
+
+
+# Part 1
+layers = get_layers(get_input())
+fewest_zeros = min(layers, key=lambda layer: layer.count('0'))
+print(fewest_zeros.count('1') * fewest_zeros.count('2'))
+
+# Part 2
 msg = ''.join(map(parse_pixel_from_layers, zip(*layers)))
 for i in range(0, PICTURE_SIZE, WIDTH):
     print(msg[i: i+WIDTH].replace('0', ' '))
