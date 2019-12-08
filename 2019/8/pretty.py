1f3485a342478319ba6740b69c23d712ecba8421
Yam Mesicka <yammesicka@gmail.com>
2019-12-08 21:25:53 +0200

    Day 8

---
+++ 2019/8/pretty.py
@@ -0,0 +1,31 @@
+import itertools
+from typing import List
+
+
+HEIGHT, WIDTH = 6, 25
+PICTURE_SIZE = HEIGHT * WIDTH
+
+
+with open('input.txt') as picture_format:
+    picture = picture_format.read().strip()
+
+layer_starts = range(0, len(picture), PICTURE_SIZE)
+layers = [picture[i:i + PICTURE_SIZE] for i in layer_starts]
+
+
+# Part 1
+fewest_zeros = min(layers, key=lambda layer: layer.count('0'))
+print(fewest_zeros.count('1') * fewest_zeros.count('2'))
+
+
+# Part 2
+def parse_pixel_from_layers(layers: List[str]) -> str:
+    try:
+        return next(itertools.dropwhile('2'.__eq__, layers))
+    except StopIteration:
+        return '2'
+
+
+msg = ''.join(map(parse_pixel_from_layers, zip(*layers)))
+for i in range(0, PICTURE_SIZE, WIDTH):
+    print(msg[i: i+WIDTH].replace('0', ' '))
