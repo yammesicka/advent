7673d6cc53891d50c2ffc7ef6a2d4f4a0bcd5d57
Yam Mesicka <yammesicka@gmail.com>
2020-12-06 08:25:44 +0200

    Solve day 6.

---
+++ 2020/6/pretty.py
@@ -0,0 +1,29 @@
+import os
+import pathlib
+from typing import Iterator, TextIO
+
+
+def read_groups(path: pathlib.Path) -> Iterator[list[str]]:
+    def read_next_group(f: TextIO):
+        lines = []
+        while (line := f.readline()) and line != os.linesep:
+            lines.append(line.rstrip())
+        return lines
+
+    with open(path) as f:
+        while group := read_next_group(f):
+            yield group
+
+
+def count_answers_a(lines: list[str]) -> int:
+    return len(set(''.join(lines)))
+
+
+def count_answers_b(lines: list[str]) -> int:
+    exists_in_all_lines = set(lines[0]).intersection(*lines[1:])
+    return len(exists_in_all_lines)
+
+
+if __name__ == '__main__':
+    print(sum(map(count_answers_a, read_groups(pathlib.Path('input.txt')))))
+    print(sum(map(count_answers_b, read_groups(pathlib.Path('input.txt')))))
