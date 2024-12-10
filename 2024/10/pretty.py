dbe5ed4337c401aa7d40488dac4222016db1c7ac
Yam Mesicka <yam@onestep.co>
2024-12-10 20:54:03 +0200

    fix: Small formatting thingies

--- 2024/10/pretty.py
+++ 2024/10/pretty.py
@@ -1,4 +1,6 @@
+from collections.abc import Iterator
 from pathlib import Path
+
 
 TEXT = (Path(__file__).parent / "input.txt").read_text().splitlines()
 GRID = {complex(i, j): int(c) for i, row in enumerate(TEXT) for j, c in enumerate(row)}
@@ -6,17 +8,18 @@ STARTS = [xy for xy, c in GRID.items() if c == 0]
 DIRECTIONS = (1, 1j, -1, -1j)
 
 
-def next_valid_steps(xy: complex, slope: int) -> list[complex]:
-    return [
-        xy + step for step in DIRECTIONS
-        if xy + step in GRID and GRID[xy + step] - slope == 1
-    ]
+def next_valid_steps(xy: complex, slope: int) -> Iterator[complex]:
+    for step in DIRECTIONS:
+        if xy + step in GRID and GRID[xy + step] - slope == 1:
+            yield xy + step
 
 
 def peaks(xy: complex, slope: int) -> set[complex]:
     if GRID[xy] == 9:
         return {xy}
-    return set().union(*(peaks(next_xy, slope + 1) for next_xy in next_valid_steps(xy, slope)))
+    return set().union(
+        *(peaks(next_xy, slope + 1) for next_xy in next_valid_steps(xy, slope))
+    )
 
 
 def routes(xy: complex, slope: int) -> int:
